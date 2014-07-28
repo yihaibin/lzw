81d48295d2d4f225386549528392ab384e7309ff
forgetman <forgetman@qq.com>
2014-07-29 00:21:45 +0800

    根据新设计图及切图: 1. 基本完成商品布局, 包括图片 2. 完成商品headview的布局

--- 芝麻妹/src/com/lzw/zmm/fragment/homepage/HomeFragment.java
+++ 芝麻妹/src/com/lzw/zmm/fragment/homepage/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.lzw.zmm.fragment.homepage;
 
-import android.content.Intent;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
+import com.androidex.util.LogMgr;
 import com.lzw.zmm.R;
 import com.lzw.zmm.base.BaseFragment;
 import com.lzw.zmm.util.res.ResLoader;
@@ -12,8 +17,15 @@ import com.lzw.zmm.view.FlowRadioGroup;
 
 public class HomeFragment extends BaseFragment {
 
+	private static final String TAG = HomeFragment.class.getSimpleName();
+	
 	private View mHeadView;
 	private View mLayoutFilterDetails;
+	
+	private ArrayList<RadioButton> mRbsClassify = new ArrayList<RadioButton>();
+	private ArrayList<RadioButton> mRbsEffect = new ArrayList<RadioButton>();
+	private ArrayList<RadioButton> mRbsPrice = new ArrayList<RadioButton>();
+	private ArrayList<RadioButton> mRbsOther = new ArrayList<RadioButton>();
 	
 	public HomeFragment() {
 	}
@@ -24,7 +36,7 @@ public class HomeFragment extends BaseFragment {
 		
 		setFragmentContentView(R.layout.home_fragment);
 		
-		startActivity(new Intent(getActivity(), ChannelActivity.class));
+//		startActivity(new Intent(getActivity(), ChannelActivity.class));
 	}
 	
 
@@ -54,39 +66,91 @@ public class HomeFragment extends BaseFragment {
 		 * 2. 可以适应以后可能出现的动态改变
 		 */
 		// 分类筛选
+		mRbsClassify.add(getRadioButton("全部"));
+		mRbsClassify.add(getRadioButton("护肤"));
+		mRbsClassify.add(getRadioButton("彩妆"));
+		mRbsClassify.add(getRadioButton("香水"));
+		mRbsClassify.add(getRadioButton("身体护理"));
+		mRbsClassify.add(getRadioButton("套装礼盒"));
+		mRbsClassify.add(getRadioButton("美妆工具"));
+		
 		FlowRadioGroup rgClassify = (FlowRadioGroup) findViewById(R.id.home_headview_classify_rg);
-		rgClassify.addView(getRadioButton("全部"));
-		rgClassify.addView(getRadioButton("护肤"));
-		rgClassify.addView(getRadioButton("彩妆"));
-		rgClassify.addView(getRadioButton("香水"));
-		rgClassify.addView(getRadioButton("身体护理"));
-		rgClassify.addView(getRadioButton("套装礼盒"));
-		rgClassify.addView(getRadioButton("美妆工具"));
-		rgClassify.check(0);
+		for (int i = 0; i < mRbsClassify.size(); ++i) {
+			rgClassify.addView(mRbsClassify.get(i));
+		}
+		rgClassify.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				LogMgr.d(TAG, "rgClassify position = " + checkedId);
+			}
+		});
+		rgClassify.check(mRbsClassify.get(0).getId());
 		
 		// 功效筛选
+		mRbsEffect.add(getRadioButton("全部"));
+		mRbsEffect.add(getRadioButton("多功效"));
+		mRbsEffect.add(getRadioButton("美白"));
+		mRbsEffect.add(getRadioButton("抗皱"));
+		mRbsEffect.add(getRadioButton("紧致"));
+		mRbsEffect.add(getRadioButton("收敛毛孔"));
+		mRbsEffect.add(getRadioButton("防晒"));
+		mRbsEffect.add(getRadioButton("去黑眼圈"));
+		mRbsEffect.add(getRadioButton("祛痘"));
+		mRbsEffect.add(getRadioButton("保湿补水"));
+		
 		FlowRadioGroup rgEffect = (FlowRadioGroup) findViewById(R.id.home_headview_effect_rg);
-		rgEffect.addView(getRadioButton("全部"));
-		rgEffect.addView(getRadioButton("多功效"));
-		rgEffect.addView(getRadioButton("美白"));
-		rgEffect.addView(getRadioButton("抗皱"));
-		rgEffect.addView(getRadioButton("紧致"));
-		rgEffect.addView(getRadioButton("收敛毛孔"));
-		rgEffect.addView(getRadioButton("防晒"));
-		rgEffect.addView(getRadioButton("去黑眼圈"));
-		rgEffect.addView(getRadioButton("祛痘"));
-		rgEffect.addView(getRadioButton("保湿补水"));
-		rgEffect.check(0);
+		for (int i = 0; i < mRbsEffect.size(); ++i) {
+			rgEffect.addView(mRbsEffect.get(i));
+		}
+		rgEffect.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				LogMgr.d(TAG, "rgEffect position = " + checkedId);
+			}
+		});
+		rgEffect.check(mRbsEffect.get(0).getId());
 		
 		// 价格筛选
+		mRbsPrice.add(getRadioButton("全部"));
+		mRbsPrice.add(getRadioButton("0-49元"));
+		mRbsPrice.add(getRadioButton("50-99元"));
+		mRbsPrice.add(getRadioButton("100-199元"));
+		mRbsPrice.add(getRadioButton("200-299元"));
+		mRbsPrice.add(getRadioButton("300元以上"));
+		
 		FlowRadioGroup rgPrice = (FlowRadioGroup) findViewById(R.id.home_headview_price_rg);
-		rgPrice.addView(getRadioButton("全部"));
-		rgPrice.addView(getRadioButton("0-49元"));
-		rgPrice.addView(getRadioButton("50-99元"));
-		rgPrice.addView(getRadioButton("100-199元"));
-		rgPrice.addView(getRadioButton("200-299元"));
-		rgPrice.addView(getRadioButton("300元以上"));
-		rgPrice.check(0);
+		for (int i = 0; i < mRbsPrice.size(); ++i) {
+			rgPrice.addView(mRbsPrice.get(i));
+		}
+		rgPrice.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				LogMgr.d(TAG, "rgPrice position = " + checkedId);
+			}
+		});
+		rgPrice.check(mRbsPrice.get(0).getId());
+		
+		// 其他筛选
+		mRbsOther.add(getRadioButton("全部"));
+		mRbsOther.add(getRadioButton("限时特卖"));
+		
+		FlowRadioGroup rgOther = (FlowRadioGroup) findViewById(R.id.home_headview_other_rg);
+		for (int i = 0; i < mRbsOther.size(); ++i) {
+			rgOther.addView(mRbsOther.get(i));
+		}
+		rgOther.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				int id = group.getCheckedRadioButtonId();
+				RadioButton rb = (RadioButton) findViewById(id);
+				LogMgr.d(TAG, "rgOther position = " + rb.getText().toString());
+			}
+		});
+		rgOther.check(mRbsOther.get(0).getId());
 	}
 
 	@Override
