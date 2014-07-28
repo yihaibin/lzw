193d12f834d81c58d8d3aa6a3771532630ffd85a
forgetman <forgetman@qq.com>
2014-07-29 00:58:19 +0800

    修复color写成dimen的bug

--- 芝麻妹/src/com/lzw/zmm/view/DragGrid.java
+++ 芝麻妹/src/com/lzw/zmm/view/DragGrid.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
 import android.os.Vibrator;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
